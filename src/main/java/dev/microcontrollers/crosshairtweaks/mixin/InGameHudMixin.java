8c593bccd60b18bafa85e2010072bb611e64a5b5
MicrocontrollersDev <microcontrollersyt@gmail.com>
2023-07-19 11:15:33 -0700

    1.19.4 backport

--- src/main/java/dev/microcontrollers/crosshairtweaks/mixin/InGameHudMixin.java
+++ src/main/java/dev/microcontrollers/crosshairtweaks/mixin/InGameHudMixin.java
@@ -5,8 +5,8 @@ import com.llamalad7.mixinextras.injector.WrapWithCondition;
 import com.mojang.blaze3d.platform.GlStateManager;
 import dev.microcontrollers.crosshairtweaks.config.CrosshairTweaksConfig;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,7 +15,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(InGameHud.class)
 public class InGameHudMixin {
 	@Inject(method = "renderCrosshair", at = @At("HEAD"), cancellable = true)
-	private void isInContainer(DrawContext context, CallbackInfo ci) {
+	private void isInContainer(MatrixStack matrices, CallbackInfo ci) {
 		if (MinecraftClient.getInstance().currentScreen != null && CrosshairTweaksConfig.hideInContainers) ci.cancel();
 	}
 
@@ -27,8 +27,7 @@ public class InGameHudMixin {
 
 	@WrapWithCondition(method = "renderCrosshair", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;blendFuncSeparate(Lcom/mojang/blaze3d/platform/GlStateManager$SrcFactor;Lcom/mojang/blaze3d/platform/GlStateManager$DstFactor;Lcom/mojang/blaze3d/platform/GlStateManager$SrcFactor;Lcom/mojang/blaze3d/platform/GlStateManager$DstFactor;)V"))
 	public boolean removeBlending(GlStateManager.SrcFactor srcFactor, GlStateManager.DstFactor dstFactor, GlStateManager.SrcFactor srcAlpha, GlStateManager.DstFactor dstAlpha) {
-		if (CrosshairTweaksConfig.removeBlending) return false;
-		else return true;
+		return !CrosshairTweaksConfig.removeBlending;
 	}
 
 }
